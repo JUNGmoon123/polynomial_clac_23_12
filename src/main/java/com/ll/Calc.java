bc82dbee025d472153eb700971711e67fb352509
JUNGmoon123 <jungpro567@gmail.com>
2023-12-27 16:18:46 +0900

    식에서불필요한괄호제거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,13 +2,10 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+    
+    // '()'괄호를 제거하기위한 메소드
     exp = stringOuterBracket(exp);
-    //-일때는 +/*둘다 false가 떠서 조건문 모두 해당하지않아서임.
-    // 이제 이 고장난 기능을 고쳐야함.
-    //전과 다른점은 ""안의 부호양옆에 공백을 추가해줬음
-    // -5를 false로 보고 - 5 일때는 true로 봐서 서로 다른부분임.
-    //만약 붙여쓸경우 ||부분에서 True가 나온다.
- //   boolean needToMultiply = exp.contains("*");
+
     boolean needToMultiply = exp.contains(" * ");
     boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
     boolean needToCompound = needToMultiply && needToPlus;
@@ -32,20 +29,7 @@ public class Calc {
 
       return sum;
     }
-
-//    else if(needToMinus){
-//      exp = exp.replaceAll("\\- ", "\\+ \\-");
-//
-//      String[] bits = exp.split(" \\+ ");
-//      int sum = 0;
-//
-//      for (int i = 0; i < bits.length; i++) {
-//        sum += Integer.parseInt(bits[i]);
-//      }
-//
-//      return sum;
-//    }
-
+    
     else if (needToMultiply) {
       String[] bits = exp.split(" \\* ");
 

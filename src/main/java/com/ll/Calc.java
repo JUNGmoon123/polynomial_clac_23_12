9279bbf2a709b392dc7d10e3de8fb257812ede4f
JUNGmoon123 <jungpro567@gmail.com>
2023-12-27 11:19:16 +0900

    10 - 10 - 10 - 10 + 10 + 10 - 10

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,32 +2,42 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
+  
+    exp = exp.replaceAll("\\- ", "\\+ \\-"); //- 를 + - 문자로 바꿈, Test에 수식에 띄어쓰기 있어서 포함시킴
 
-    boolean needToPlus = exp.contains("+");   //contains 많이 쓸것 같으니 꼭 기억
-    boolean needToMinus = exp.contains("-");
+    //replaceAll 로인해 true/false가 필요없어짐. 밑에 if조건문 true판단이 필요없어짐.
+//    boolean needToPlus = exp.contains("+");   //contains 많이 쓸것 같으니 꼭 기억
+//    boolean needToMinus = exp.contains("-");
 
-    String[] bits = null;
-
-    if (needToPlus) {
-      bits = exp.split(" \\+ ");    //메타문자  역슬래쉬 필요
-    } else if (needToMinus) {
-      bits = exp.split(" - ");      // -는 역슬래쉬 필요없음.
+   // String[] bits = null;
+    String[] bits = exp.split(" \\+ ");   //항상 쪼개기전에 띄어쓰기공간 똑같이 해줄것.
+//    if (needToPlus) {
+//      bits = exp.split(" \\+ ");    //메타문자  역슬래쉬 필요
+//    } else if (needToMinus) {
+//      bits = exp.split(" - ");      // -는 역슬래쉬 필요없음.
+//    }
+    int[] num = null;
+    int sum = 0;
+    for(int i = 0; i<bits.length; i++){
+      sum +=  Integer.parseInt(bits[i]);
     }
+//    int a = Integer.parseInt(bits[0]);
+//    int b = Integer.parseInt(bits[1]);
+//    int c = 0;
+//
+//    if (bits.length > 2){
+//      c = Integer.parseInt(bits[2]);
+//    }
 
-    int a = Integer.parseInt(bits[0]);
-    int b = Integer.parseInt(bits[1]);
-    int c = 0;
+    return sum;
 
-    if (bits.length > 2){
-      c = Integer.parseInt(bits[2]);
-    }
+//   if(needToPlus){
+//      return a + b + c;
+//    }
+//    else if (needToMinus) {
+//      return a - b - c;
+//    }
 
-    if (needToPlus) {
-      return a + b + c;
-    } else if (needToMinus) {
-      return a - b - c;
-    }
-
-    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+    //throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,7 +4,7 @@ package com.ll;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-
+    System.out.println("10 - 10 - 10 - 10 + 10 + 10 - 10".replaceAll("\\- ", "\\+ \\-"));
 
   }
 }

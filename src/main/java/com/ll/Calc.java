41c6903eb2f2f74c74d56ffe13ae3e62a57dd950
JUNGmoon123 <jungpro567@gmail.com>
2023-12-27 14:00:29 +0900

    10 + 5 * 2

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,42 +2,39 @@ package com.ll;
 
 public class Calc {
   public static int run(String exp) {
-  
-    exp = exp.replaceAll("\\- ", "\\+ \\-"); //- 를 + - 문자로 바꿈, Test에 수식에 띄어쓰기 있어서 포함시킴
+    boolean needToMultiply = exp.contains("*");
+    boolean needToPlus = exp.contains("+");
+    //boolean needToMinus = exp.contains("-");
+    boolean needToCompound = needToMultiply && needToPlus;
 
-    //replaceAll 로인해 true/false가 필요없어짐. 밑에 if조건문 true판단이 필요없어짐.
-//    boolean needToPlus = exp.contains("+");   //contains 많이 쓸것 같으니 꼭 기억
-//    boolean needToMinus = exp.contains("-");
-
-   // String[] bits = null;
-    String[] bits = exp.split(" \\+ ");   //항상 쪼개기전에 띄어쓰기공간 똑같이 해줄것.
-//    if (needToPlus) {
-//      bits = exp.split(" \\+ ");    //메타문자  역슬래쉬 필요
-//    } else if (needToMinus) {
-//      bits = exp.split(" - ");      // -는 역슬래쉬 필요없음.
-//    }
-    int[] num = null;
-    int sum = 0;
-    for(int i = 0; i<bits.length; i++){
-      sum +=  Integer.parseInt(bits[i]);
+    //재귀함수
+    if(needToCompound && needToMultiply){
+      String[] bits = exp.split(" \\+ ");
+      return Integer.parseInt(bits[0]) + run(bits[1]);
     }
-//    int a = Integer.parseInt(bits[0]);
-//    int b = Integer.parseInt(bits[1]);
-//    int c = 0;
-//
-//    if (bits.length > 2){
-//      c = Integer.parseInt(bits[2]);
-//    }
+    if (needToPlus) {
+      exp = exp.replaceAll("\\- ", "\\+ \\-");
 
-    return sum;
+      String[] bits = exp.split(" \\+ ");
 
-//   if(needToPlus){
-//      return a + b + c;
-//    }
-//    else if (needToMinus) {
-//      return a - b - c;
-//    }
+      int sum = 0;
 
-    //throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+      for (int i = 0; i < bits.length; i++) {
+        sum += Integer.parseInt(bits[i]);
+      }
+
+      return sum;
+    }
+    else if (needToMultiply) {
+      String[] bits = exp.split(" \\* ");
+
+      int rs = 1;
+
+      for (int i = 0; i < bits.length; i++) {
+          rs *= Integer.parseInt(bits[i]);
+      }
+      return rs;
+    }
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
